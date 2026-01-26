ff2d0cbb516ba910c2f21e7010b599d4a8665c84
Adelando <adelando@users.noreply.github.com>
2026-01-26 11:53:41 +0800

    Update config_flow.py

--- custom-components/long_form_word_countdown/config_flow.py
+++ custom-components/long_form_word_countdown/config_flow.py
@@ -1,36 +1,25 @@
-import voluptuous as vol
+import vol
 from homeassistant import config_entries
-from homeassistant.helpers.selector import (
-    DateTimeSelector,
-    IconSelector,
-    TextSelector,
-)
-from .const import DOMAIN, CONF_TARGET_DATE
+from homeassistant.helpers.selector import DateTimeSelector, IconSelector, TextSelector, BooleanSelector
+from .const import DOMAIN, CONF_TARGET_DATE, CONF_AFTER_TIMER, CONF_FLASH_ZERO
 
 class LongFormCountdownConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Long Form Word Countdown."""
-
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
         errors = {}
-
         if user_input is not None:
-            return self.async_create_entry(
-                title=user_input["name"], 
-                data=user_input
-            )
+            if user_input.get(CONF_AFTER_TIMER) and user_input.get(CONF_FLASH_ZERO):
+                errors["base"] = "cannot_select_both"
+            else:
+                return self.async_create_entry(title=user_input["name"], data=user_input)
 
-        # Define the UI Schema
         data_schema = vol.Schema({
             vol.Required("name"): TextSelector(),
             vol.Required(CONF_TARGET_DATE): DateTimeSelector(),
             vol.Optional("icon", default="mdi:timer-sand"): IconSelector(),
+            vol.Optional(CONF_AFTER_TIMER, default=True): BooleanSelector(),
+            vol.Optional(CONF_FLASH_ZERO, default=False): BooleanSelector(),
         })
 
-        return self.async_show_form(
-            step_id="user", 
-            data_schema=data_schema, 
-            errors=errors
-        )
+        return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
